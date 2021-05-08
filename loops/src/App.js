f9ffa5452da3ee7cb33b96f226c4d9eee489b155
André de Albuquerque <itstheandre@gmail.com>
2021-05-08 15:34:21 +0200

    Ooopsie daisy

---
+++ loops/src/App.js
@@ -0,0 +1,90 @@
+import React from "react";
+import "./App.css";
+import arrayOfFriends from "./utils/friends";
+
+/* 
+in express 
+res.render(pagName, {users: []})
+
+in handlebars
+{{#each users}}
+{{this.name}}
+
+{{/each}}
+
+*/
+
+function App() {
+  const [friendsArr, setFriendsArr] = React.useState(arrayOfFriends);
+  // const stateAndFunction = React.useState(arrayOfFriends);
+  // const [sortingVersion, setSortingVersion] = React.useState("asc");
+
+  function sortAsc() {
+    // const newArr = [...stateAndFunction[0]].sort((a, b) => {
+    const newArr = [...friendsArr].sort((a, b) => {
+      return a.firstName.localeCompare(b.firstName);
+    });
+
+    setFriendsArr(newArr);
+    // stateAndFunction[1](newArr);
+    // setSortingVersion("asc");
+  }
+
+  function sortDesc() {
+    // // const newArr = [...stateAndFunction[0]].sort((a, b) => {
+    const newArr = [...friendsArr].sort((a, b) => {
+      return b.firstName.localeCompare(a.firstName);
+    });
+
+    setFriendsArr(newArr);
+    // // stateAndFunction[1](newArr);
+    // setSortingVersion("desc");
+  }
+
+  // const newArr = [...friendsArr].sort((a, b) => {
+  //   if (sortingVersion === "asc") {
+  //     return a.firstName.localeCompare(b.firstName);
+  //   }
+
+  //   return b.firstName.localeCompare(a.firstName);
+  // });
+
+  return (
+    <div className="App">
+      <h1>List of friends</h1>
+      <button onClick={sortAsc}>SORT ASC</button>
+      <div />
+      <button onClick={sortDesc}>SORT DESC</button>
+      <div />
+      {/* <button >GET JUST THREE NAMES</button> */}
+      {/* {stateAndFunction[0].map((friend) => { */}
+      {friendsArr.map((friend) => {
+        return (
+          <Person
+            key={friend.id}
+            // key={index}
+            // firstName={friend.firstName}
+            // lastName={friend.lastName}
+            // username={friend.username}
+            {...friend}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+function Person(props) {
+  return (
+    <div>
+      <p>
+        Name: {props.firstName} {props.lastName}
+      </p>
+      <p style={{ fontSize: ".75rem" }}>@{props.username}</p>
+    </div>
+  );
+}
+
+export default App;
+
+// fontSize: ".75rem"
---
+++ loops/src/utils/friends.js
@@ -0,0 +1,35 @@
+const arrayOfFriends = [
+  {
+    firstName: "Chandler",
+    lastName: "Bing",
+    id: 1,
+    username: "chanandlerbong",
+  },
+  {
+    firstName: "Joey",
+    lastName: "Tribiani",
+    id: 2,
+    username: "Joseph",
+  },
+  {
+    firstName: "Ross",
+    lastName: "Geller",
+    id: 3,
+    username: "We were on a break",
+  },
+  { firstName: "Monica", lastName: "Geller", id: 4, username: "Crazy" },
+  {
+    firstName: "Rachel",
+    lastName: "Green",
+    id: 5,
+    username: "raquel",
+  },
+  {
+    firstName: "Phoebe",
+    lastName: "Buffay",
+    id: 6,
+    username: "Phoebs",
+  },
+];
+
+export default arrayOfFriends;
